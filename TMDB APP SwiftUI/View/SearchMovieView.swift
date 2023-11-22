52feb506bccf1e658ff62c8eb3737850874b4d64
Tanvir Rahman <tanvir27.58@gmail.com>
2023-11-23 04:39:14 +0600

    Loader issue

--- TMDB APP SwiftUI/View/SearchMovieView.swift
+++ TMDB APP SwiftUI/View/SearchMovieView.swift
@@ -36,22 +36,23 @@ struct SearchMovieView: View {
                     RoundedRectangle(cornerRadius: 10)
                         .stroke(Color.secondary, lineWidth: 1)
                 )
-
-            ScrollView {
-                if (viewModel.searchData?.results.count) ?? 0 >= 1 && viewModel.responseCode == 200{
-                    VStack {
-                        ForEach(viewModel.searchData!.results, id: \.title) { data in
-                            MovieCell(imageURL: data.poster_path ?? "", title: data.title ?? "", overview: data.overview ?? "", rating: String(data.vote_average ?? 0.0), isLoading: false)
-                        }
-                    }
-                }else if viewModel.responseCode != 200 && !isLoading && viewModel.responseCode != 0{
-                    Text("Search Error")
-                        .font(.largeTitle).foregroundColor(.primary)
-                }
-                
-                if viewModel.isLoading {
+            if viewModel.isLoading {
+                ScrollView {
                     ForEach(0..<10){ _ in
                         MovieCell(imageURL: "", title: ReductedData.original_title, overview: ReductedData.overview, rating: ReductedData.vote_average, isLoading: viewModel.isLoading)
+                    }
+                }
+            }else {
+                ScrollView {
+                    if (viewModel.searchData?.results.count) ?? 0 >= 1 && viewModel.responseCode == 200{
+                        VStack {
+                            ForEach(viewModel.searchData!.results, id: \.title) { data in
+                                MovieCell(imageURL: data.poster_path ?? "", title: data.title ?? "", overview: data.overview ?? "", rating: String(data.vote_average ?? 0.0), isLoading: false)
+                            }
+                        }
+                    }else if viewModel.responseCode != 200 && !isLoading && viewModel.responseCode != 0{
+                        Text("Search Error")
+                            .font(.largeTitle).foregroundColor(.primary)
                     }
                 }
             }
@@ -60,6 +61,10 @@ struct SearchMovieView: View {
                 UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
             }
         .edgesIgnoringSafeArea(.bottom)
+        .onAppear{
+            viewModel.fetchData(url: "https://api.themoviedb.org/3/search/movie?api_key=38e61227f85671163c275f9bd95a8803&query=marvel")
+            viewModel.isLoading = true
+        }
     }
 }
 
