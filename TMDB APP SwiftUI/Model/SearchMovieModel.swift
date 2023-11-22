3a7aeed0256d88be68ec45631c74c74040fe845c
Tanvir Rahman <tanvir27.58@gmail.com>
2023-11-23 01:49:04 +0600

    Search movie model

--- TMDB APP SwiftUI/Model/SearchMovieModel.swift
+++ TMDB APP SwiftUI/Model/SearchMovieModel.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+struct Search: Codable {
+    let page: Int?
+    let results: [MovieDetails]
+    let total_pages: Int?
+    let total_results: Int?
+}
+struct MovieDetails: Codable {
+    let adult: Bool?
+    let original_title: String?
+    let overview: String?
+    let poster_path: String?
+    let title: String?
+    let vote_average: Double?
+}
