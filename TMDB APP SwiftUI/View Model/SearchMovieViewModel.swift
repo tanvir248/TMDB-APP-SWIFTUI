9ecd3f2597c113f2801c0d26ab8b429c9b244707
Tanvir Rahman <tanvir27.58@gmail.com>
2023-11-23 02:09:25 +0600

    Search Movie View Model

--- TMDB APP SwiftUI/View Model/SearchMovieViewModel.swift
+++ TMDB APP SwiftUI/View Model/SearchMovieViewModel.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+enum errorSearch: Error {
+    case invalidURL
+    
+}
+
+class SearchMovieViewModel: ObservableObject {
+    @Published var searchData: [Search] = []
+    
+    func fetchData(url: String){
+        guard let url = URL(string: "www.www.com") else {
+                    print("URL Error")
+                  return
+              }
+        var request = URLRequest(url: url)
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        request.httpMethod = "GET"
+        URLSession.shared.dataTask(with: request) {(data, response, error) in
+            guard let data = data, error == nil else {
+                print("Data Error: No Data found!!")
+                return
+            }
+            guard let searchResults = try? JSONDecoder().decode(Search.self, from: data) else {
+                print("Failed to Decode Data")
+                return
+            }
+            print(searchResults)
+        }.resume()
+    }
+   
+}
